6b2fb6b1bdc22454de56b31d5efbef61b8442e9c
Benjamin Tautfest <b.tautfest@gmx.de>
2020-10-07 17:17:29 +0200

    Change header

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,11 @@ const bookMarkNav = document.querySelector('[data-js=bookmarks-nav]')
 const createNav = document.querySelector('[data-js=create-nav]')
 const profileNav = document.querySelector('[data-js=profile-nav]')
 
+const homeHeader = document.querySelector('[data-js=home-header]')
+const bookmarksHeader = document.querySelector('[data-js=bookmarks-header]')
+const createHeader = document.querySelector('[data-js=create-header]')
+const profileHeader = document.querySelector('[data-js=profile-header]')
+
 const home = document.querySelector('[data-js=home]')
 const bookmarks = document.querySelector('[data-js=bookmarks]')
 const create = document.querySelector('[data-js=create]')
@@ -15,6 +20,11 @@ homeNav.addEventListener('click', () => {
   home.classList.remove('d-none')
   create.classList.add('d-none')
   profile.classList.add('d-none')
+
+  homeHeader.classList.remove('d-none')
+  bookmarksHeader.classList.add('d-none')
+  createHeader.classList.add('d-none')
+  profileHeader.classList.add('d-none')
 })
 
 bookMarkNav.addEventListener('click', () => {
@@ -22,6 +32,11 @@ bookMarkNav.addEventListener('click', () => {
   home.classList.add('d-none')
   create.classList.add('d-none')
   profile.classList.add('d-none')
+
+  homeHeader.classList.add('d-none')
+  bookmarksHeader.classList.remove('d-none')
+  createHeader.classList.add('d-none')
+  profileHeader.classList.add('d-none')
 })
 
 createNav.addEventListener('click', () => {
@@ -29,6 +44,11 @@ createNav.addEventListener('click', () => {
   bookmarks.classList.add('d-none')
   home.classList.add('d-none')
   profile.classList.add('d-none')
+
+  homeHeader.classList.add('d-none')
+  bookmarksHeader.classList.add('d-none')
+  createHeader.classList.remove('d-none')
+  profileHeader.classList.add('d-none')
 })
 
 profileNav.addEventListener('click', () => {
@@ -36,4 +56,9 @@ profileNav.addEventListener('click', () => {
   bookmarks.classList.add('d-none')
   home.classList.add('d-none')
   profile.classList.remove('d-none')
+
+  homeHeader.classList.add('d-none')
+  bookmarksHeader.classList.add('d-none')
+  createHeader.classList.add('d-none')
+  profileHeader.classList.remove('d-none')
 })
